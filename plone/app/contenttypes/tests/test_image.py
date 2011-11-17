d8727eef30d80dc2a9d5067e037f6d16d3c8107d
Timo Stollenwerk <timo@zmag.de>
2011-11-17 03:51:26 +0100

    Fix image test.

--- plone/app/contenttypes/tests/test_image.py
+++ plone/app/contenttypes/tests/test_image.py
@@ -12,7 +12,7 @@ from plone.app.contenttypes.testing import \
 from plone.app.testing import TEST_USER_ID, setRoles
 
 
-class DocumentTest(unittest.TestCase):
+class ImageTest(unittest.TestCase):
 
     layer = PLONE_APP_CONTENTTYPES_INTEGRATION_TESTING
 
@@ -23,43 +23,43 @@ class DocumentTest(unittest.TestCase):
 
     def test_schema(self):
         fti = queryUtility(IDexterityFTI,
-                           name='Document')
+                           name='Image')
         schema = fti.lookupSchema()
          
         self.assertEquals(schema.__module__,
                           'plone.dexterity.schema.generated')
         self.assertEquals(schema.__name__,
-                          'plone_0_Document')
+                          'plone_0_Image')
 
     def test_fti(self):
         fti = queryUtility(IDexterityFTI,
-                           name='Document')
+                           name='Image')
         
         self.assertNotEquals(None, fti)
 
     def test_factory(self):
         fti = queryUtility(IDexterityFTI,
-                           name='Document')
+                           name='Image')
         factory = fti.factory
-        document = createObject(factory)
+        image = createObject(factory)
         
-        self.assertEquals(str(type(document)),
+        self.assertEquals(str(type(image)),
                           "<class 'plone.dexterity.content.Item'>")
 
     def test_adding(self):
-        self.portal.invokeFactory('Document',
-                                  'document')
-        document = self.portal['document']
+        self.portal.invokeFactory('Image',
+                                  'image')
+        image = self.portal['image']
         
-        self.assertEquals(str(type(aq_base(document))),
+        self.assertEquals(str(type(aq_base(image))),
                           "<class 'plone.dexterity.content.Item'>")
 
     def test_view(self):
-        self.portal.invokeFactory('Document', 'document')
-        document = self.portal['document']
-        self.request.set('URL', document.absolute_url())
-        self.request.set('ACTUAL_URL', document.absolute_url())        
-        view = document.restrictedTraverse('@@view')
+        self.portal.invokeFactory('Image', 'image')
+        image = self.portal['image']
+        self.request.set('URL', image.absolute_url())
+        self.request.set('ACTUAL_URL', image.absolute_url())        
+        view = image.restrictedTraverse('@@view')
 
         self.failUnless(view())
         self.assertEquals(view.request.response.status, 200)
