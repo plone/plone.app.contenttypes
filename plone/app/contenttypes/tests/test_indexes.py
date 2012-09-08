f0f21a0f50f385e2d80bdb1c1acea0f8bd28b303
Timo Stollenwerk <contact@timostollenwerk.net>
2012-09-08 09:34:20 +0200

    Fix indent.

--- plone/app/contenttypes/tests/test_indexes.py
+++ plone/app/contenttypes/tests/test_indexes.py
@@ -11,38 +11,36 @@ from plone.app.testing import TEST_USER_ID, setRoles
 
 class CatalogIntegrationTest(unittest.TestCase):
 
-   layer = PLONE_APP_CONTENTTYPES_INTEGRATION_TESTING
+    layer = PLONE_APP_CONTENTTYPES_INTEGRATION_TESTING
 
-   def setUp(self):
-       self.portal = self.layer['portal']
-       setRoles(self.portal, TEST_USER_ID, ['Manager'])
-       self.portal.invokeFactory('Folder', 'folder')
-       self.folder = self.portal.folder
-       self.folder.invokeFactory('Document',
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Folder', 'folder')
+        self.folder = self.portal.folder
+        self.folder.invokeFactory('Document',
                                  'document')
-       self.document = self.folder.document
-       self.catalog = getToolByName(self.portal, 'portal_catalog')
+        self.document = self.folder.document
+        self.catalog = getToolByName(self.portal, 'portal_catalog')
 
-   def test_title_in_searchable_text_index(self):
-       self.document.title = "My title"
-       self.document.reindexObject()
-       brains = self.catalog.searchResults(dict(
-           SearchableText="My title",
-           portal_type="Document"
-           ))
-       self.assertEqual(len(brains), 1)
-       self.assertEquals(brains[0].getPath(),
-           '/plone/folder/document')
+    def test_title_in_searchable_text_index(self):
+        self.document.title = "My title"
+        self.document.reindexObject()
+        brains = self.catalog.searchResults(dict(
+            SearchableText="My title",
+            portal_type="Document"
+        ))
+        self.assertEqual(len(brains), 1)
+        self.assertEquals(brains[0].getPath(),
+            '/plone/folder/document')
 
-   def test_description_in_searchable_text_index(self):
-       self.document.description = "My description"
-       self.document.reindexObject()
-       brains = self.catalog.searchResults(dict(
-           SearchableText="My description",
-           portal_type="Document"
-           ))
-       self.assertEqual(len(brains), 1)
-       self.assertEquals(brains[0].getPath(),
-           '/plone/folder/document')
-
-
+    def test_description_in_searchable_text_index(self):
+        self.document.description = "My description"
+        self.document.reindexObject()
+        brains = self.catalog.searchResults(dict(
+            SearchableText="My description",
+            portal_type="Document"
+        ))
+        self.assertEqual(len(brains), 1)
+        self.assertEquals(brains[0].getPath(),
+            '/plone/folder/document')
