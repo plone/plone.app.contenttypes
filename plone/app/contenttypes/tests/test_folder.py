7ea5e521a32ddf79f4053bce375b60494665031f
Timo Stollenwerk <contact@timostollenwerk.net>
2012-11-20 09:45:13 +0100

    Pep8.

--- plone/app/contenttypes/tests/test_folder.py
+++ plone/app/contenttypes/tests/test_folder.py
@@ -19,50 +19,67 @@ class FolderTest(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer['portal']
         self.request = self.layer['request']
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])        
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
 
     def test_schema(self):
-        fti = queryUtility(IDexterityFTI,
-                           name='Folder')
+        fti = queryUtility(
+            IDexterityFTI,
+            name='Folder'
+        )
         schema = fti.lookupSchema()
-         
-        self.assertEquals(schema.__module__,
-                          'plone.dexterity.schema.generated')
-        self.assertEquals(schema.__name__,
-                          'plone_0_Folder')
+
+        self.assertEquals(
+            schema.__module__,
+            'plone.dexterity.schema.generated'
+        )
+        self.assertEquals(
+            schema.__name__,
+            'plone_0_Folder'
+        )
 
     def test_fti(self):
-        fti = queryUtility(IDexterityFTI,
-                           name='Folder')
-        
+        fti = queryUtility(
+            IDexterityFTI,
+            name='Folder'
+        )
+
         self.assertNotEquals(None, fti)
 
     def test_factory(self):
-        fti = queryUtility(IDexterityFTI,
-                           name='Folder')
+        fti = queryUtility(
+            IDexterityFTI,
+            name='Folder'
+        )
         factory = fti.factory
         folder = createObject(factory)
-        
-        self.assertEquals(str(type(folder)),
-                          "<class 'plone.dexterity.content.Container'>")
+
+        self.assertEquals(
+            str(type(folder)),
+            "<class 'plone.dexterity.content.Container'>"
+        )
 
     def test_adding(self):
-        self.portal.invokeFactory('Folder',
-                                  'folder')
+        self.portal.invokeFactory(
+            'Folder',
+            'folder'
+        )
         folder = self.portal['folder']
-        
-        self.assertEquals(str(type(aq_base(folder))),
-                          "<class 'plone.dexterity.content.Container'>")
+
+        self.assertEquals(
+            str(type(aq_base(folder))),
+            "<class 'plone.dexterity.content.Container'>"
+        )
 
     def test_view(self):
         self.portal.invokeFactory('Folder', 'folder')
         folder = self.portal['folder']
         self.request.set('URL', folder.absolute_url())
-        self.request.set('ACTUAL_URL', folder.absolute_url())        
+        self.request.set('ACTUAL_URL', folder.absolute_url())
         view = folder.restrictedTraverse('@@view')
 
         self.failUnless(view())
         self.assertEquals(view.request.response.status, 200)
+
 
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
