be58a6e5b917a66bb97ff7d1ba86cd8b4a9c8acf
Timo Stollenwerk <contact@timostollenwerk.net>
2012-11-23 12:06:54 +0100

    Document functional test added.

--- plone/app/contenttypes/tests/test_document.py
+++ plone/app/contenttypes/tests/test_document.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from Acquisition import aq_base
 import unittest2 as unittest
 
@@ -6,13 +7,19 @@ from zope.component import queryUtility
 
 from plone.dexterity.interfaces import IDexterityFTI
 
-from plone.app.contenttypes.testing import \
-    PLONE_APP_CONTENTTYPES_INTEGRATION_TESTING
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.testing.z2 import Browser
+
+from plone.app.contenttypes.testing import (
+    PLONE_APP_CONTENTTYPES_INTEGRATION_TESTING,
+    PLONE_APP_CONTENTTYPES_FUNCTIONAL_TESTING
+)
 
 from plone.app.testing import TEST_USER_ID, setRoles
 
 
-class DocumentTest(unittest.TestCase):
+class DocumentIntegrationTest(unittest.TestCase):
 
     layer = PLONE_APP_CONTENTTYPES_INTEGRATION_TESTING
 
@@ -62,6 +69,42 @@ class DocumentTest(unittest.TestCase):
 
         self.failUnless(view())
         self.assertEquals(view.request.response.status, 200)
+
+
+class DocumentFunctionalText(unittest.TestCase):
+
+    layer = PLONE_APP_CONTENTTYPES_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        app = self.layer['app']
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        self.portal_url = self.portal.absolute_url()
+        self.browser = Browser(app)
+        self.browser.handleErrors = False
+        self.browser.addHeader(
+            'Authorization',
+            'Basic %s:%s' % (SITE_OWNER_NAME, SITE_OWNER_PASSWORD,)
+        )
+
+    def test_add_document(self):
+        self.browser.open(self.portal_url)
+        self.browser.getLink('Page').click()
+        self.assertTrue('Title' in self.browser.contents)
+        self.assertTrue('Description' in self.browser.contents)
+        self.assertTrue('Text' in self.browser.contents)
+        self.browser.getControl(name='form.widgets.IDublinCore.title')\
+        .value = "My document"
+        self.browser.getControl(name='form.widgets.IDublinCore.description')\
+        .value = "This is my document."
+        self.browser.getControl(name='form.widgets.text')\
+        .value = "Lorem Ipsum"
+        self.browser.getControl('Save').click()
+        self.assertTrue(self.browser.url.endswith('my-document/view'))
+        self.assertTrue('My document' in self.browser.contents)
+        self.assertTrue('This is my document' in self.browser.contents)
+        self.assertTrue('Lorem Ipsum' in self.browser.contents)
+
 
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
