f24f7fac858dbcb05f3d2bb4346fbca4e871bc2d
Timo Stollenwerk <contact@timostollenwerk.net>
2012-11-23 19:51:25 +0100

    Browser views for document, file and event added.

--- plone/app/contenttypes/interfaces.py
+++ plone/app/contenttypes/interfaces.py
@@ -2,6 +2,16 @@
 from zope.interface import Interface, Attribute
 
 
+class IPloneAppContenttypesLayer(Interface):
+    """Marker interface that defines a ZTK browser layer. We can reference
+    this in the 'layer' attribute of ZCML <browser:* /> directives to ensure
+    the relevant registration only takes effect when this theme is installed.
+
+    The browser layer is installed via the browserlayer.xml GenericSetup
+    import step.
+    """
+
+
 class IDocument(Interface):
     """
     """
