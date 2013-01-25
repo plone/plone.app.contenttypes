b0d901499b8a0713e992dad4bb1f27e5ccd01cf1
Alexander Loechel <AlexanderLoechel@yahoo.de>
2013-01-25 15:08:53 +0100

    change permission add alt tag add two tests for mime icon

--- plone/app/contenttypes/browser/utils.py
+++ plone/app/contenttypes/browser/utils.py
@@ -39,6 +39,10 @@ class Utils(BrowserView):
         mime.append(mtr.lookup("application/octet-stream")[0])
 
         return portal_url + "/" + guess_icon_path(mime[0])
+        
+        ## function works but is possibly not best implementation. following code 
+        ## might work for files where the mimetype is not directly recognized
+        
 #        if len(mime) > 0:
 #            icon = portal_url + "/" + guess_icon_path(mime[0])
 #        else: 
