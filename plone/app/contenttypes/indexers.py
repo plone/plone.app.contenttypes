a3219975a1662a8eb0e62b3d3e089cff2fff1dd1
Timo Stollenwerk <contact@timostollenwerk.net>
2013-02-05 09:11:59 +0100

    Pep8

--- plone/app/contenttypes/indexers.py
+++ plone/app/contenttypes/indexers.py
@@ -5,7 +5,7 @@ from plone.indexer.decorator import indexer
 
 from plone.app.contenttypes.interfaces import (
     IEvent, IDocument, INewsItem, ILink
-        )
+)
 
 
 @indexer(IEvent)
@@ -17,21 +17,25 @@ def start_date(obj):
 def end_date(obj):
     return DateTime(IEvent(obj).end_date)
 
+
 def SearchableText(obj, text=False):
-    return ' '.join((obj.id, obj.title, obj.description, 
-                ))
+    return ' '.join((obj.id, obj.title, obj.description, ))
+
 
 @indexer(INewsItem)
 def SearchableText_news(obj):
     return ' '.join((SearchableText(obj), getattr(obj.text, 'output', '')))
 
+
 @indexer(IDocument)
 def SearchableText_document(obj):
     return ' '.join((SearchableText(obj), getattr(obj.text, 'output', '')))
 
+
 @indexer(ILink)
 def SearchableText_link(obj):
     return ' '.join((SearchableText(obj), obj.remoteUrl))
+
 
 @indexer(ILink)
 def getRemoteUrl(obj):
