1a4b3ba2a5a39d82b5e041fd6d14041b1a145b5c
Timo Stollenwerk <contact@timostollenwerk.net>
2013-03-12 21:20:53 +0100

    Upgrade step to move fti.model_file to schema folder.

---
+++ plone/app/contenttypes/tests/test_upgrade.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+import unittest2 as unittest
+
+from zope.component import createObject
+from zope.component import queryUtility
+
+from plone.dexterity.interfaces import IDexterityFTI
+
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.testing.z2 import Browser
+
+from plone.app.testing import TEST_USER_ID, setRoles
+
+from plone.app.contenttypes.interfaces import IFolder
+
+from plone.app.contenttypes.testing import (
+    PLONE_APP_CONTENTTYPES_INTEGRATION_TESTING,
+    PLONE_APP_CONTENTTYPES_FUNCTIONAL_TESTING
+)
+
+from plone.app.contenttypes.upgrades import update_fti
+
+
+class UpgradeTo1000IntegrationTest(unittest.TestCase):
+
+    layer = PLONE_APP_CONTENTTYPES_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        self.request['ACTUAL_URL'] = self.portal.absolute_url()
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_update_fti_document(self):
+        fti = queryUtility(
+            IDexterityFTI,
+            name='Document'
+        )
+        fti.model_file = 'plone.app.contenttypes:document.xml'
+
+        update_fti(self.portal)
+
+        fti = queryUtility(
+            IDexterityFTI,
+            name='Document'
+        )
+        self.assertEqual(
+            fti.model_file, 
+            'plone.app.contenttypes.schema:document.xml'
+        )
+
+    def test_update_fti_event(self):
+        fti = queryUtility(
+            IDexterityFTI,
+            name='Event'
+        )
+        fti.model_file = 'plone.app.contenttypes:event.xml'
+
+        update_fti(self.portal)
+
+        fti = queryUtility(
+            IDexterityFTI,
+            name='Event'
+        )
+        self.assertEqual(
+            fti.model_file, 
+            'plone.app.contenttypes.schema:event.xml'
+        )
+
+    def test_update_fti_file(self):
+        fti = queryUtility(
+            IDexterityFTI,
+            name='File'
+        )
+        fti.model_file = 'plone.app.contenttypes:file.xml'
+
+        update_fti(self.portal)
+
+        fti = queryUtility(
+            IDexterityFTI,
+            name='File'
+        )
+        self.assertEqual(
+            fti.model_file, 
+            'plone.app.contenttypes.schema:file.xml'
+        )
+
+    def test_update_fti_folder(self):
+        fti = queryUtility(
+            IDexterityFTI,
+            name='Folder'
+        )
+        fti.model_file = 'plone.app.contenttypes:folder.xml'
+
+        update_fti(self.portal)
+
+        fti = queryUtility(
+            IDexterityFTI,
+            name='Folder'
+        )
+        self.assertEqual(
+            fti.model_file, 
+            'plone.app.contenttypes.schema:folder.xml'
+        )
+
+    def test_update_fti_image(self):
+        fti = queryUtility(
+            IDexterityFTI,
+            name='File'
+        )
+        fti.model_file = 'plone.app.contenttypes:image.xml'
+
+        update_fti(self.portal)
+
+        fti = queryUtility(
+            IDexterityFTI,
+            name='File'
+        )
+        self.assertEqual(
+            fti.model_file, 
+            'plone.app.contenttypes.schema:file.xml'
+        )
+
+    def test_update_fti_link(self):
+        fti = queryUtility(
+            IDexterityFTI,
+            name='Link'
+        )
+        fti.model_file = 'plone.app.contenttypes:link.xml'
+
+        update_fti(self.portal)
+
+        fti = queryUtility(
+            IDexterityFTI,
+            name='Link'
+        )
+        self.assertEqual(
+            fti.model_file, 
+            'plone.app.contenttypes.schema:link.xml'
+        )
+
+    def test_update_fti_news_item(self):
+        fti = queryUtility(
+            IDexterityFTI,
+            name='News Item'
+        )
+        fti.model_file = 'plone.app.contenttypes:news_item.xml'
+
+        update_fti(self.portal)
+
+        fti = queryUtility(
+            IDexterityFTI,
+            name='News Item'
+        )
+        self.assertEqual(
+            fti.model_file, 
+            'plone.app.contenttypes.schema:news_item.xml'
+        )
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
---
+++ plone/app/contenttypes/upgrades.py
@@ -0,0 +1,50 @@
+from Products.CMFCore.utils import getToolByName
+
+from zope.component import queryUtility
+
+from plone.dexterity.interfaces import IDexterityFTI
+
+
+def update_fti(context):
+    # Document
+    fti = queryUtility(
+        IDexterityFTI,
+        name='Document'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:document.xml"
+    # Event
+    fti = queryUtility(
+        IDexterityFTI,
+        name='Event'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:event.xml"
+    # File
+    fti = queryUtility(
+        IDexterityFTI,
+        name='File'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:file.xml"
+    # Folder
+    fti = queryUtility(
+        IDexterityFTI,
+        name='Folder'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:folder.xml"
+    # Image
+    fti = queryUtility(
+        IDexterityFTI,
+        name='Image'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:image.xml"
+    # Link
+    fti = queryUtility(
+        IDexterityFTI,
+        name='Link'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:link.xml"
+    # News Item
+    fti = queryUtility(
+        IDexterityFTI,
+        name='News Item'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:news_item.xml"
