6f35f38e79068aa89a0b9404be8da4bfa1600c70
Timo Stollenwerk <contact@timostollenwerk.net>
2013-04-04 16:33:56 +0200

    UTF-8 header check added to pre-commit hook.

--- templates/code-analysis.sh
+++ templates/code-analysis.sh
@@ -4,14 +4,14 @@ RED='\033[00;31m'
 GREEN='\033[00;32m'
 
 function success {
-    echo -ne $1
+    echo -ne "$1"
     echo -e " [ \033[00;32mOK\033[0m ]"
 }
 
 function failure {
-    echo -ne $1
+    echo -ne "$1"
     echo -e " [ $${RED}FAILURE$${RESTORE} ]"
     echo "$${@:2}"
 }
 
-log=$$(${options['bin']} ${options['directory']}) && success ${options['title']} || failure ${options['title']} $$log
+log=$$(${options['run']}) && success ${options['title']} || failure ${options['title']} "$$log"
