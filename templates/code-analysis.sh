b9bb5e76ca3b8be715d9e6d0fa103f863a0d2dfb
Timo Stollenwerk <contact@timostollenwerk.net>
2013-04-06 00:46:43 +0200

    Style code analysis output.

--- templates/code-analysis.sh
+++ templates/code-analysis.sh
@@ -5,11 +5,27 @@ GREEN='\033[00;32m'
 
 function success {
     echo -ne "$1"
+    len=$${#1}
+    gap=$$((20-$$len))
+    i=0
+    while [ $$i -le $$gap ]
+    do
+       echo -ne " "
+       i=$$(( $$i + 1 ))
+    done
     echo -e " [ \033[00;32mOK\033[0m ]"
 }
 
 function failure {
     echo -ne "$1"
+    len=$${#1}
+    gap=$$((15-$$len))
+    i=0
+    while [ $$i -le $$gap ]
+    do
+       echo -ne " "
+       i=$$(( $$i + 1 ))
+    done
     echo -e " [ $${RED}FAILURE$${RESTORE} ]"
     echo "$${@:2}"
 }
