880049133f70b8692dcdced158bb7d401f66b9ef
Timo Stollenwerk <contact@timostollenwerk.net>
2013-04-06 14:22:48 +0200

    Add check for code-analysis to skip csslint/jslint if they are not installed properly.

--- templates/code-analysis.sh
+++ templates/code-analysis.sh
@@ -3,9 +3,28 @@ RESTORE='\033[0m'
 RED='\033[00;31m'
 GREEN='\033[00;32m'
 
+title="${options['title']}"
+
+function check {
+    command -v ${options.get('check', '')} >/dev/null 2>&1 || {
+        echo -ne $$title
+        #echo >&2 "${options.get('check', '')} not installed!";
+        len=$${#title}
+        gap=$$((18-$$len))
+        i=0
+        while [ $$i -le $$gap ]
+        do
+           echo -ne " "
+           i=$$(( $$i + 1 ))
+        done
+        echo -e " [ \033[00;33mSKIP\033[0m ]"
+        exit 1;
+    }
+}
+
 function success {
-    echo -ne "$1"
-    len=$${#1}
+    echo -ne $$title
+    len=$${#title}
     gap=$$((20-$$len))
     i=0
     while [ $$i -le $$gap ]
@@ -17,8 +36,8 @@ function success {
 }
 
 function failure {
-    echo -ne "$1"
-    len=$${#1}
+    echo -ne $$title
+    len=$${#title}
     gap=$$((15-$$len))
     i=0
     while [ $$i -le $$gap ]
@@ -27,7 +46,8 @@ function failure {
        i=$$(( $$i + 1 ))
     done
     echo -e " [ $${RED}FAILURE$${RESTORE} ]"
-    echo "$${@:2}"
+    echo "$${@:1}"
 }
 
-log=$$(${options['run']}) && success "${options['title']}" || failure "${options['title']}" "$$log"
+check
+log=$$(${options['run']}) && success || failure "$$log"
