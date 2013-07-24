526539b2eed8a7981088426c5f7fc43823b51018
Érico Andrei <erico@simplesconsultoria.com.br>
2013-07-24 15:42:20 -0300

    Add subscribers to deal with empty title or description for File and Image content types. Fixes #54

---
+++ plone/app/contenttypes/subscribers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from plone.app.contenttypes.interfaces import IImage
+
+
+def set_title_description(obj, event):
+    ''' Sets title to filename if no title
+        was provided.
+        Also sets an empty unicode as description if
+        no description was provided.
+    '''
+    title = obj.title
+    if not title:
+        if IImage.providedBy(obj):
+            datafield = obj.image
+        else:
+            datafield = obj.file
+        filename = datafield.filename
+        obj.title = filename
+
+    description = obj.description
+    if not description:
+        obj.description = u''
