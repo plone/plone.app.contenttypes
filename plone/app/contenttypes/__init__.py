60da6c44a671779e406eaef50b60a42dbf115b64
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-08-23 17:33:10 +0200

    fix(permissions): Add a permission for each content type

--- plone/app/contenttypes/__init__.py
+++ plone/app/contenttypes/__init__.py
@@ -1,3 +1,4 @@
 from zope.i18nmessageid import MessageFactory
+import permissions
 
 _ = MessageFactory('plone')
---
+++ plone/app/contenttypes/permissions.py
@@ -0,0 +1,32 @@
+from AccessControl.SecurityInfo import ModuleSecurityInfo
+from Products.CMFCore.permissions import setDefaultRoles
+#http://developer.plone.org/security/custom_permissions.html
+security = ModuleSecurityInfo('plone.app.contenttypes')
+TYPE_ROLES = ('Manager', 'Site Administrator', 'Owner')
+perms = []
+import logging
+for typename in (
+    "Collection",
+    "Document",
+    "Event",
+    "File",
+    "Folder",
+    "Image",
+    "Link",
+    "News Item",
+):
+#    ctype = "plone.app.contenttypes." + typename
+    permid = 'Add' + typename
+    permname = 'plone.app.contenttypes: Add ' + typename
+    security.declarePublic(permid)
+    setDefaultRoles(permname, TYPE_ROLES)
+    logging.getLogger("log").info("setDefaultRoles(%s, %s)" % (permname, TYPE_ROLES))
+
+AddCollection = "plone.app.contenttypes: Add Collection"
+AddDocument = "plone.app.contenttypes: Add Document"
+AddEvent = "plone.app.contenttypes: Add Event"
+AddFile = "plone.app.contenttypes: Add File"
+AddFolder = "plone.app.contenttypes: Add Folder"
+AddImage = "plone.app.contenttypes: Add Image"
+AddLink = "plone.app.contenttypes: Add Link"
+AddNewsItem = "plone.app.contenttypes: Add News Item"
