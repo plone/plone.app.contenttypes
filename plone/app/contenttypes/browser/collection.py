060616f473a10d45667deffa3f6e0804b4c29741
Maurits van Rees <maurits@vanrees.org>
2013-11-11 16:39:31 +0100

    Put selectedViewFields on the collection view.

--- plone/app/contenttypes/browser/collection.py
+++ plone/app/contenttypes/browser/collection.py
@@ -15,3 +15,13 @@ class CollectionView(BrowserView):
         context = aq_inner(self.context)
         wrapped = ICollection(context)
         return wrapped.getFoldersAndImages(**kwargs)
+
+    def selectedViewFields(self):
+        """Returns a list of all metadata fields from the catalog that were
+           selected.
+
+        Note: this is supported by plone.app.collection, but not yet
+        in plone.app.contenttypes.  See the commented out
+        customViewFields field.
+        """
+        return []
