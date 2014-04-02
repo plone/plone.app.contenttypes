9aa51ee0432c35076d60d47f5971d213b92c79fd
Philip Bauer <bauer@starzel.de>
2014-04-02 12:24:50 +0200

    reuse convenience-list DEFAULT_TYPES

--- plone/app/contenttypes/permissions.py
+++ plone/app/contenttypes/permissions.py
@@ -1,21 +1,12 @@
 # -*- coding: utf-8 -*-
 from AccessControl.SecurityInfo import ModuleSecurityInfo
 from Products.CMFCore.permissions import setDefaultRoles
+from plone.app.contenttypes.utils import DEFAULT_TYPES
 #http://developer.plone.org/security/custom_permissions.html
 security = ModuleSecurityInfo('plone.app.contenttypes')
 TYPE_ROLES = ('Manager', 'Site Administrator', 'Owner', 'Contributor')
 perms = []
-for typename in (
-    "Collection",
-    "Document",
-    "Event",
-    "File",
-    "Folder",
-    "Image",
-    "Link",
-    "News Item",
-):
-#    ctype = "plone.app.contenttypes." + typename
+for typename in DEFAULT_TYPES:
     permid = 'Add' + typename
     permname = 'plone.app.contenttypes: Add ' + typename
     security.declarePublic(permid)
