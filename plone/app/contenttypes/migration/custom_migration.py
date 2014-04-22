b4eee833b49bc46e0cdbd720b9760d716c84c79d
Gauthier Bastien <g.bastien@imio.be>
2014-04-22 20:14:41 +0200

    Added base methods listing AT and DX content types, added default template for custom content types migration based on atct content type migration template

--- plone/app/contenttypes/migration/custom_migration.py
+++ plone/app/contenttypes/migration/custom_migration.py
@@ -13,19 +13,35 @@ class CustomMigrationForm(BrowserView):
         return self.template()
 
     def getATFTIs(self):
-        ''' return a list of all at-types with existing instances
-        (including default-types)  '''
-        pass
+        '''Returns a list of all AT types with existing instances (including default-types).'''
+        results = []
+        archetype_tool = getToolByName(self.context, 'archetype_tool', None)
+        # if we do not have archetype_tool, it means that we have no registered AT types
+        if not archetype_tool:
+            return results
+
+        typesTool = getToolByName(self.context, 'portal_types')
+        catalog = getToolByName(self.context, 'portal_catalog')
+        registeredTypeNames = [registered['name'] for registered in archetype_tool.listRegisteredTypes()]
+        for fti in typesTool.listTypeInfo():
+            ftiId = fti.getId()
+            if hasattr(fti, 'content_meta_type') and \
+               fti.content_meta_type in registeredTypeNames and \
+               catalog(portal_type=ftiId):
+                results.append({'id': ftiId,
+                                'title': fti.Title()})
+        return results
 
     def getDXFTIs(self):
-        '''return the FTI's of all DX-Types (including default-types)
-        '''
+        '''return the FTI's of all DX-Types (including default-types).'''
         results = []
         portal = self.context
         ttool = getToolByName(portal, 'portal_types')
-        for ti in ttool.listTypeInfo():
-            if IDexterityFTI.providedBy(ti):
-                results.append(ti)
+        for fti in ttool.listTypeInfo():
+            if IDexterityFTI.providedBy(fti):
+                results.append({'id': fti.getId(),
+                                'title': fti.Title()})
+        return results
 
     def getFieldsForATType(self, typename):
         pass
