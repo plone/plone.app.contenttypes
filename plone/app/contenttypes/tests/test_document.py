a323e6115f83ae33f56f3987dd0aecb3d623d564
Philip Bauer <bauer@starzel.de>
2014-11-02 11:32:01 +0000

    sort imports

--- plone/app/contenttypes/tests/test_document.py
+++ plone/app/contenttypes/tests/test_document.py
@@ -1,28 +1,23 @@
 # -*- coding: utf-8 -*-
-import unittest2 as unittest
-
-from zope.interface import alsoProvides
-from zope.component import createObject
-from zope.component import queryUtility
-
-from plone.dexterity.interfaces import IDexterityFTI
-
+from plone.app.contenttypes.interfaces import IDocument
+from plone.app.contenttypes.testing import \
+    PLONE_APP_CONTENTTYPES_FUNCTIONAL_TESTING
+from plone.app.contenttypes.testing import \
+    PLONE_APP_CONTENTTYPES_INTEGRATION_TESTING
 from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import SITE_OWNER_PASSWORD
-from plone.testing.z2 import Browser
-
-from plone.app.textfield.value import RichTextValue
-
-from plone.app.contenttypes.interfaces import IDocument
-
-from plone.app.contenttypes.testing import (
-    PLONE_APP_CONTENTTYPES_INTEGRATION_TESTING,
-    PLONE_APP_CONTENTTYPES_FUNCTIONAL_TESTING
-)
-
 from plone.app.testing import TEST_USER_ID, setRoles
+from plone.app.textfield.value import RichTextValue
 from plone.app.z3cform.interfaces import IPloneFormLayer
+from plone.dexterity.interfaces import IDexterityFTI
+from plone.testing.z2 import Browser
+from zope.component import createObject
+from zope.component import queryUtility
+from zope.interface import alsoProvides
+
 import transaction
+import unittest2 as unittest
+
 
 class DocumentIntegrationTest(unittest.TestCase):
 
