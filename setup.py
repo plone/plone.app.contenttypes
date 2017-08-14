1ec3e668ddd3922b37b708976299361ffe56c03c
Maurits van Rees <maurits@vanrees.org>
2017-08-14 12:30:32 +0200

    Preparing release 1.1.4
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 import sys
 
-version = '1.1.4.dev0'
+version = '1.1.4'
 additional_requires = []
 if sys.version_info < (2, 7):
     additional_requires.append('importlib')
