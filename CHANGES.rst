e3252a8e52e308c691fdbb75b7d3cbca3c654ae1
esteele <eric@esteele.net>
2017-08-27 16:46:10 -0400

    Preparing release 1.4.2
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.4.2 (unreleased)
+1.4.2 (2017-08-27)
 ------------------
 
 New features:
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 import os
 
 
-version = '1.4.2.dev0'
+version = '1.4.2'
 
 
 def read(*rnames):
