44e52137fd44bced4ea5d9accb387112beb30c2f
Peter Mathis <petschki@users.noreply.github.com>
2021-02-13 09:57:11 +0100

    Barceloneta LTS support (#588)
    * add helper to group mimetypes into useful icon groups
    
    * fix outdated pull classes
    
    * update markup for boostrap
    
    * update button classes for bootstrap
    
    * fix icon lookup with fallback
    
    * update markup for file ct
    
    * use icon resolver in link view
    
    * cleanup
    
    * add icon resolver, cleanup
    
    * Use the bootstrap way for striped tables
    
    * bootstrap b1 changes
    
    * bootstrap b1 changes
    
    * use python statements in file-template
    
    * add changenote
    
    * Use python statements in newsitem-template
    
    * add changenote
    
    * Use python statements in newsitem-template (2)
    
    * Update document template to python calls
    
    * Remove Archetypes warning viewlet
    
    * update tal statements to python at the end of the file
    
    * updated tal-statements to python calls
    
    * no_items_message should be a property (same as for folders)
    
    * use python-statements in listing template
    
    * Remove recently added fallback in newsitem-template for text attribute (this attribute is more or less mandatory)
    
    * Use python statements in listing_tabular-template
    
    * Remove unused define of item_description in listing_tabular-template
    
    * fix download-link. remove path-statements
    
    * fix tests
    
    * fix new listing selector
    
    * restore albums and fix tests
    
    * reimplement albums
    
    * major version bump
    
    * add icon_expr definitions
    
    Co-authored-by: Peter Holzer <peter.holzer@agitator.com>
    Co-authored-by: Stefan Antonelli <stefan.antonelli@operun.de>
    Co-authored-by: ale-rt <alessandro.pisa@gmail.com>
    Co-authored-by: Philip Bauer <bauer@starzel.de>
    Co-authored-by: Marcel Liebischer <liebischer@interaktiv.de>
    Co-authored-by: ladplone <ladplone@gmail.com>

--- plone/app/contenttypes/browser/file.py
+++ plone/app/contenttypes/browser/file.py
@@ -2,6 +2,7 @@
 from plone.app.contenttypes.browser.utils import Utils
 from Products.CMFPlone.utils import human_readable_size
 
+import re
 
 class FileView(Utils):
 
@@ -18,3 +19,19 @@ class FileView(Utils):
 
     def human_readable_size(self):
         return human_readable_size(self.context.file.getSize())
+
+    def icon(self):
+        if "text" in self.context.file.contentType:
+            return "contenttype-text"
+        if "html" in self.context.file.contentType:
+            return "contenttype-text"
+        if re.search(r"(archive|zip|compressed)", self.context.file.contentType):
+            return "contenttype-archive"
+        if "audio" in self.context.file.contentType:
+            return "contenttype-audio"
+        if "video" in self.context.file.contentType:
+            return "contenttype-video"
+        if "pdf" in self.context.file.contentType:
+            return "contenttype-pdf"
+        else:
+            return "contenttype-file"
