cb1de7d74eb1a4888a0a32e39e3366cab5901c00
Jens W. Klein <jk@kleinundpartner.at>
2022-04-06 17:15:11 +0200

    cleanup aftermath of #3395 + isort/black

--- plone/app/contenttypes/browser/folder.py
+++ plone/app/contenttypes/browser/folder.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
 from Acquisition import aq_base
 from Acquisition import aq_inner
 from plone.app.contenttypes import _
 from plone.app.contenttypes.behaviors.leadimage import ILeadImage
-from plone.app.vocabularies.metadatafields import get_field_label
 from plone.app.contenttypes.interfaces import IFolder
 from plone.app.contenttypes.interfaces import IImage
+from plone.app.vocabularies.metadatafields import get_field_label
+from plone.base.batch import Batch
+from plone.base.interfaces import ISecuritySchema
+from plone.base.interfaces import ISiteSchema
 from plone.event.interfaces import IEvent
 from plone.memoize.view import memoize
 from plone.registry.interfaces import IRegistry
-from Products.CMFPlone.interfaces import ISiteSchema
-from Products.CMFPlone.PloneBatch import Batch
 from Products.CMFPlone.utils import safe_callable
 from Products.Five import BrowserView
 from zope.component import getMultiAdapter
@@ -18,13 +18,6 @@ from zope.component import getUtility
 from zope.contentprovider.interfaces import IContentProvider
 
 import random
-
-
-HAS_SECURITY_SETTINGS = True
-try:
-    from Products.CMFPlone.interfaces import ISecuritySchema
-except ImportError:
-    HAS_SECURITY_SETTINGS = False
 
 
 class FolderView(BrowserView):
@@ -38,8 +31,7 @@ class FolderView(BrowserView):
     def plone_view(self):
         if not self._plone_view:
             self._plone_view = getMultiAdapter(
-                (self.context, self.request),
-                name=u'plone'
+                (self.context, self.request), name=u"plone"
             )
         return self._plone_view
 
@@ -47,8 +39,7 @@ class FolderView(BrowserView):
     def portal_state(self):
         if not self._portal_state:
             self._portal_state = getMultiAdapter(
-                (self.context, self.request),
-                name=u'plone_portal_state'
+                (self.context, self.request), name=u"plone_portal_state"
             )
         return self._portal_state
 
@@ -56,20 +47,22 @@ class FolderView(BrowserView):
     def pas_member(self):
         if not self._pas_member:
             self._pas_member = getMultiAdapter(
-                (self.context, self.request),
-                name=u'pas_member'
+                (self.context, self.request), name=u"pas_member"
             )
         return self._pas_member
 
     @property
     def b_size(self):
-        b_size = getattr(self.request, 'b_size', None)\
-            or getattr(self.request, 'limit_display', None) or 20
+        b_size = (
+            getattr(self.request, "b_size", None)
+            or getattr(self.request, "limit_display", None)
+            or 20
+        )
         return int(b_size)
 
     @property
     def b_start(self):
-        b_start = getattr(self.request, 'b_start', None) or 0
+        b_start = getattr(self.request, "b_start", None) or 0
         return int(b_start)
 
     def results(self, **kwargs):
@@ -85,28 +78,22 @@ class FolderView(BrowserView):
                 sequence.
         """
         # Extra filter
-        kwargs.update(self.request.get('contentFilter', {}))
-        if 'object_provides' not in kwargs:  # object_provides is more specific
-            kwargs.setdefault('portal_type', self.friendly_types)
-        kwargs.setdefault('batch', True)
-        kwargs.setdefault('b_size', self.b_size)
-        kwargs.setdefault('b_start', self.b_start)
-        kwargs.setdefault('orphan', 1)
+        kwargs.update(self.request.get("contentFilter", {}))
+        if "object_provides" not in kwargs:  # object_provides is more specific
+            kwargs.setdefault("portal_type", self.friendly_types)
+        kwargs.setdefault("batch", True)
+        kwargs.setdefault("b_size", self.b_size)
+        kwargs.setdefault("b_start", self.b_start)
+        kwargs.setdefault("orphan", 1)
 
-        listing = aq_inner(self.context).restrictedTraverse(
-            '@@folderListing', None)
+        listing = aq_inner(self.context).restrictedTraverse("@@folderListing", None)
         if listing is None:
             return []
         results = listing(**kwargs)
         return results
 
     def batch(self):
-        batch = Batch(
-            self.results(),
-            size=self.b_size,
-            start=self.b_start,
-            orphan=1
-        )
+        batch = Batch(self.results(), size=self.b_size, start=self.b_start, orphan=1)
         return batch
 
     def normalizeString(self, text):
@@ -130,116 +117,106 @@ class FolderView(BrowserView):
     @property
     def use_view_action(self):
         registry = getUtility(IRegistry)
-        return registry.get('plone.types_use_view_action_in_listings', [])
+        return registry.get("plone.types_use_view_action_in_listings", [])
 
     @property
     def show_about(self):
-        if not HAS_SECURITY_SETTINGS:
-            # BBB
-            site_props = self.context.restrictedTraverse(
-                'portal_properties').site_properties
-            show_about = getattr(site_props, 'allowAnonymousViewAbout', False)
-        else:
-            registry = getUtility(IRegistry)
-            settings = registry.forInterface(ISecuritySchema, prefix='plone')
-            show_about = getattr(settings, 'allow_anon_views_about', False)
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(ISecuritySchema, prefix="plone")
+        show_about = getattr(settings, "allow_anon_views_about", False)
         return show_about or not self.isAnon
 
     @property
     def text(self):
-        textfield = getattr(aq_base(self.context), 'text', None)
-        text = textfield.output_relative_to(self.context)\
-            if getattr(textfield, 'output_relative_to', None)\
+        textfield = getattr(aq_base(self.context), "text", None)
+        text = (
+            textfield.output_relative_to(self.context)
+            if getattr(textfield, "output_relative_to", None)
             else None
+        )
         if text:
-            self.text_class = 'stx' if textfield.mimeType in (
-                'text/structured', 'text/x-rst', 'text/restructured'
-            ) else 'plain'
+            self.text_class = (
+                "stx"
+                if textfield.mimeType
+                in ("text/structured", "text/x-rst", "text/restructured")
+                else "plain"
+            )
         return text
 
     @property
     def tabular_fields(self):
         ret = []
-        ret.append('Title')
+        ret.append("Title")
         if self.show_about:
-            ret.append('Creator')
-        ret.append('Type')
+            ret.append("Creator")
+        ret.append("Type")
         if self.show_about:
-            ret.append('ModificationDate')
+            ret.append("ModificationDate")
         return ret
 
     def tabular_field_label(self, field):
         """Return the internationalized label (Message object) corresponding
-           to the field.
+        to the field.
         """
         return get_field_label(field)
 
     def tabular_fielddata(self, item, fieldname):
-        value = getattr(item, fieldname, '')
+        value = getattr(item, fieldname, "")
         if safe_callable(value):
             value = value()
         if fieldname in [
-                'CreationDate',
-                'ModificationDate',
-                'Date',
-                'EffectiveDate',
-                'ExpirationDate',
-                'effective',
-                'expires',
-                'start',
-                'end',
-                'created',
-                'modified',
-                'last_comment_date']:
+            "CreationDate",
+            "ModificationDate",
+            "Date",
+            "EffectiveDate",
+            "ExpirationDate",
+            "effective",
+            "expires",
+            "start",
+            "end",
+            "created",
+            "modified",
+            "last_comment_date",
+        ]:
             value = self.toLocalizedTime(value, long_format=1)
 
         return {
             # 'title': _(fieldname, default=fieldname),
-            'value': value
+            "value": value
         }
 
     def is_event(self, obj):
-        if getattr(obj, 'getObject', False):
+        if getattr(obj, "getObject", False):
             obj = obj.getObject()
         return IEvent.providedBy(obj)
 
     def formatted_date(self, item):
         provider = getMultiAdapter(
-            (self.context, self.request, self),
-            IContentProvider, name='formatted_date'
+            (self.context, self.request, self), IContentProvider, name="formatted_date"
         )
         return provider(item)
 
     @property
     @memoize
     def album_images(self):
-        """Get all images within this folder.
-        """
+        """Get all images within this folder."""
         provides = [
             IImage.__identifier__,
             ILeadImage.__identifier__,
         ]
-        images = self.results(
-            batch=False,
-            object_provides=provides
-        )
+        images = self.results(batch=False, object_provides=provides)
         return images
 
     @property
     @memoize
     def album_folders(self):
-        """Get all folders within this folder.
-        """
-        images = self.results(
-            batch=False,
-            object_provides=IFolder.__identifier__
-        )
+        """Get all folders within this folder."""
+        images = self.results(batch=False, object_provides=IFolder.__identifier__)
         return images
 
     @property
     def album_random_image(self):
-        """Get random image from this folder.
-        """
+        """Get random image from this folder."""
         img = None
         images = self.album_images
         if images:
@@ -248,59 +225,55 @@ class FolderView(BrowserView):
 
     @property
     def album_number_images(self):
-        """Get number of images from this folder.
-        """
+        """Get number of images from this folder."""
         images = self.album_images
         return len(images)
 
     @property
     def no_items_message(self):
         return _(
-            'description_no_items_in_folder',
-            default=u'There are currently no items in this folder.'
+            "description_no_items_in_folder",
+            default=u"There are currently no items in this folder.",
         )
 
     @memoize
     def get_thumb_scale_table(self):
-        if getattr(self.context, 'suppress_thumbs', False):
+        if getattr(self.context, "suppress_thumbs", False):
             return None
-        thsize = getattr(self.context, 'thumb_scale_table', None)
+        thsize = getattr(self.context, "thumb_scale_table", None)
         if thsize:
             return thsize
         registry = getUtility(IRegistry)
-        settings = registry.forInterface(
-            ISiteSchema, prefix='plone', check=False)
+        settings = registry.forInterface(ISiteSchema, prefix="plone", check=False)
         if settings.no_thumbs_tables:
             return None
         return settings.thumb_scale_table
 
     @memoize
     def get_thumb_scale_list(self):
-        if getattr(self.context, 'suppress_thumbs', False):
+        if getattr(self.context, "suppress_thumbs", False):
             return None
-        thsize = getattr(self.context, 'thumb_scale_list', None)
+        thsize = getattr(self.context, "thumb_scale_list", None)
         if thsize:
             return thsize
         registry = getUtility(IRegistry)
-        settings = registry.forInterface(
-            ISiteSchema, prefix='plone', check=False)
+        settings = registry.forInterface(ISiteSchema, prefix="plone", check=False)
         if settings.no_thumbs_lists:
             return None
         return settings.thumb_scale_listing
 
     @memoize
     def get_thumb_scale_summary(self):
-        if getattr(self.context, 'suppress_thumbs', False):
+        if getattr(self.context, "suppress_thumbs", False):
             return None
-        thsize = getattr(self.context, 'thumb_scale_summary', None)
+        thsize = getattr(self.context, "thumb_scale_summary", None)
         if thsize:
             return thsize
         registry = getUtility(IRegistry)
-        settings = registry.forInterface(
-            ISiteSchema, prefix='plone', check=False)
+        settings = registry.forInterface(ISiteSchema, prefix="plone", check=False)
         if settings.no_thumbs_summary:
             return None
         return settings.thumb_scale_summary
 
     def show_icons(self):
-        return not getattr(self.context, 'suppress_icons', False)
+        return not getattr(self.context, "suppress_icons", False)
--- plone/app/contenttypes/browser/link_redirect_view.py
+++ plone/app/contenttypes/browser/link_redirect_view.py
@@ -1,36 +1,33 @@
-# -*- coding: utf-8 -*-
 from plone.app.contenttypes.utils import replace_link_variables_by_paths
 from plone.app.uuid.utils import uuidToObject
+from plone.base.interfaces import ITypesSchema
 from plone.registry.interfaces import IRegistry
 from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.interfaces import ITypesSchema
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from urllib.parse import urlparse
 from zope.component import getUtility
 
-import six
-
 
 # links starting with these URL scheme should not be redirected to
 NON_REDIRECTABLE_URL_SCHEMES = [
-    'mailto:',
-    'tel:',
-    'callto:',  # nonstandard according to RFC 3966. used for skype.
-    'webdav:',
-    'caldav:'
+    "mailto:",
+    "tel:",
+    "callto:",  # nonstandard according to RFC 3966. used for skype.
+    "webdav:",
+    "caldav:",
 ]
 
 # links starting with these URL scheme should not be resolved to paths
 NON_RESOLVABLE_URL_SCHEMES = NON_REDIRECTABLE_URL_SCHEMES + [
-    'file:',
-    'ftp:',
+    "file:",
+    "ftp:",
 ]
 
 
 class LinkRedirectView(BrowserView):
 
-    index = ViewPageTemplateFile('templates/link.pt')
+    index = ViewPageTemplateFile("templates/link.pt")
 
     def _url_uses_scheme(self, schemes, url=None):
         url = url or self.context.remoteUrl.strip()
@@ -41,67 +38,61 @@ class LinkRedirectView(BrowserView):
 
     def __call__(self):
         """Redirect to the Link target URL, if and only if:
-         - redirect_links property is enabled in
-           configuration registry
-         - the link is of a redirectable type (no mailto:, etc)
-         - AND current user doesn't have permission to edit the Link"""
+        - redirect_links property is enabled in
+          configuration registry
+        - the link is of a redirectable type (no mailto:, etc)
+        - AND current user doesn't have permission to edit the Link"""
         context = self.context
-        mtool = getToolByName(context, 'portal_membership')
+        mtool = getToolByName(context, "portal_membership")
 
         registry = getUtility(IRegistry)
-        settings = registry.forInterface(ITypesSchema, prefix='plone')
+        settings = registry.forInterface(ITypesSchema, prefix="plone")
         redirect_links = settings.redirect_links
 
-        can_edit = mtool.checkPermission('Modify portal content', context)
-        redirect_links = redirect_links\
-            and not self._url_uses_scheme(NON_REDIRECTABLE_URL_SCHEMES)
+        can_edit = mtool.checkPermission("Modify portal content", context)
+        redirect_links = redirect_links and not self._url_uses_scheme(
+            NON_REDIRECTABLE_URL_SCHEMES
+        )
 
         if redirect_links and not can_edit:
-            target_url = self.absolute_target_url()
-            if six.PY2:
-                target_url = target_url.encode('utf-8')
-            return self.request.RESPONSE.redirect(target_url)
-        else:
-            return self.index()
+            return self.request.RESPONSE.redirect(self.absolute_target_url())
+        return self.index()
 
     def url(self):
-        """Returns the url with link variables replaced.
-        """
+        """Returns the url with link variables replaced."""
         url = replace_link_variables_by_paths(
-            self.context,
-            self.context.remoteUrl.strip()
+            self.context, self.context.remoteUrl.strip()
         )
         return url
 
     def display_link(self):
-        """Format the url for display
-        """
+        """Format the url for display"""
 
         url = self.url()
-        if 'resolveuid' in url:
-            uid = url.split('/')[-1]
+        if "resolveuid" in url:
+            uid = url.split("/")[-1]
             obj = uuidToObject(uid)
             if obj:
                 title = obj.Title()
-                meta = '/'.join(obj.getPhysicalPath()[2:])
-                if not meta.startswith('/'):
-                    meta = '/' + meta
+                meta = "/".join(obj.getPhysicalPath()[2:])
+                if not meta.startswith("/"):
+                    meta = "/" + meta
                 return {
-                    'title': title,
-                    'meta': meta,
+                    "title": title,
+                    "meta": meta,
                 }
 
         parsed = urlparse(url)
-        if parsed.scheme == 'mailto':
+        if parsed.scheme == "mailto":
             return {
-                'title': parsed.path,
-                'meta': '',
+                "title": parsed.path,
+                "meta": "",
             }
 
         return {
-                'title': url,
-                'meta': '',
-            }
+            "title": url,
+            "meta": "",
+        }
 
     def absolute_target_url(self):
         """Compute the absolute target URL."""
@@ -111,19 +102,14 @@ class LinkRedirectView(BrowserView):
             # For non http/https url schemes, there is no path to resolve.
             return url
 
-        if url.startswith('.'):
+        if url.startswith("."):
             # we just need to adapt ../relative/links, /absolute/ones work
             # anyway -> this requires relative links to start with ./ or
             # ../
-            context_state = self.context.restrictedTraverse(
-                '@@plone_context_state'
-            )
-            url = '/'.join([
-                context_state.canonical_object_url(),
-                url
-            ])
+            context_state = self.context.restrictedTraverse("@@plone_context_state")
+            url = "/".join([context_state.canonical_object_url(), url])
         else:
-            if not url.startswith(('http://', 'https://')):
-                url = self.request['SERVER_URL'] + url
+            if not url.startswith(("http://", "https://")):
+                url = self.request["SERVER_URL"] + url
 
         return url
