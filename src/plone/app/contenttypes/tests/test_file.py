7bb03e8ec6c6bd0e645f445b0755e85a51afb158
Maurits van Rees <maurits@vanrees.org>
2026-06-08 16:30:10 +0200

    Set max_length in image and file content types.
    Max 1024 for title, max 10000 for description.
    For the other types (using IBasic), this is being changed in plone.app.dexterity.

--- src/plone/app/contenttypes/tests/test_file.py
+++ src/plone/app/contenttypes/tests/test_file.py
@@ -164,6 +164,32 @@ class FileFunctionalTest(unittest.TestCase):
         content = self.portal[content_id]
         self.assertTrue(content.Title(), filename[:1024])
 
+    def test_long_title(self):
+        self.browser.open(self.portal_url)
+        self.browser.getLink("File").click()
+        widget = "form.widgets.description"
+        self.browser.getControl(name=widget).value = "L" * 10001
+        file_path = os.path.join(os.path.dirname(__file__), "image.jpg")
+        file_ctl = self.browser.getControl(name="form.widgets.file")
+        with io.FileIO(file_path, "rb") as f:
+            file_ctl.add_file(f, "image/png", "image.jpg")
+        self.browser.getControl("Save").click()
+        self.assertTrue("There were some errors." in self.browser.contents)
+        self.assertTrue("Value is too long" in self.browser.contents)
+
+    def test_long_description(self):
+        self.browser.open(self.portal_url)
+        self.browser.getLink("File").click()
+        widget = "form.widgets.title"
+        self.browser.getControl(name=widget).value = "L" * 1025
+        file_path = os.path.join(os.path.dirname(__file__), "image.jpg")
+        file_ctl = self.browser.getControl(name="form.widgets.file")
+        with io.FileIO(file_path, "rb") as f:
+            file_ctl.add_file(f, "image/png", "image.jpg")
+        self.browser.getControl("Save").click()
+        self.assertTrue("There were some errors." in self.browser.contents)
+        self.assertTrue("Value is too long" in self.browser.contents)
+
     def test_mime_icon_pdf_for_file_(self):
         self.browser.open(self.portal_url)
         self.browser.getLink("File").click()
